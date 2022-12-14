d0a2aa0f9d2b2967d0f50423a9251dbdbca93854
Dylan Anthony <dbanty@users.noreply.github.com>
2022-12-14 10:07:45 -0700

    feat: Add tracing integration

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
 use std::net::Ipv4Addr;
 
 use axum::Server;
+use tracing::info;
 
 use my_subgraph::app;
 
 #[tokio::main]
 async fn main() {
+    tracing_subscriber::fmt::init();
     let app = app();
     let port = std::env::var("PORT")
         .unwrap_or_else(|_| "4001".to_string())
         .parse::<u16>()
         .unwrap();
-    println!(
+    info!(
         "Run `rover dev --url http://localhost:{port} --name {crate_name}`",
         crate_name = env!("CARGO_PKG_NAME")
     );
